3d8a08fdd76b52d4821bab3de8d998e6be35ff73
Pandolfo45 <80552473+Pandolfo45@users.noreply.github.com>
2022-03-07 14:21:34 -0600

    Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+ 
   {
     path: '',
     redirectTo: 'folder/Inbox',
@@ -10,16 +11,7 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
-    path: 'folder2/:id',
-    loadChildren: () => import('./folder2/folder2.module').then( m => m.Folder2PageModule)
-  },
-  {
-    path: 'folder3/:id',
-    loadChildren: () => import('./folder3/folder3.module').then( m => m.Folder3PageModule)
-  },
-  
+  },  
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
@@ -37,7 +29,7 @@ const routes: Routes = [
   
   {
     path:'salud',
-    loadChildren:() => import('./salud/salud.component').then(m => m.SaludComponent)
+    loadChildren:() => import('./salud/salud.module').then(m => m.SaludPageModule)
   },
   
   {
@@ -65,21 +57,18 @@ const routes: Routes = [
     loadChildren: () => import('./salud3/salud3.module').then( m => m.Salud3PageModule)
   },
   {
-    path: 'folder2',
-    loadChildren: () => import('./folder2/folder2.module').then( m => m.Folder2PageModule)
-  },
-  {
-    path: 'folder3',
-    loadChildren: () => import('./folder3/folder3.module').then( m => m.Folder3PageModule)
-  },
-  {
     path: 'customers2',
     loadChildren: () => import('./customers2/customers2.module').then( m => m.Customers2PageModule)
   },
   {
     path: 'customers3',
     loadChildren: () => import('./customers3/customers3.module').then( m => m.Customers3PageModule)
+  },
+  {
+    path: 'salud',
+    loadChildren: () => import('./salud/salud.module').then( m => m.SaludPageModule)
   }
+
   
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,27 +5,40 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  
-  public appPages1 = [
-    
-    { title: 'Ximopanolti', url: '/folder/folder.module', icon: 'home' },
-    { title: 'Kali', url: '/home', icon: 'hand-left' },
-    { title: 'Tlapoua', url: '/numeros', icon: 'calculator' },
-    { title: 'Tlapali', url: '/customers', icon: 'color-palette' },
-    { title: 'salud', url: '/salud',icon:'heart-half'},
-  ];
-  public appPages2 = [
-    { title: 'Ximopanolti', url: '/folder2/folder2.component', icon: 'home' },
-    { title: 'Kali', url: '/home2', icon: 'hand-left' },
-    { title: 'Tlapoua', url: '/numeros2', icon: 'calculator' },
-    { title: 'Tlapali', url: '/customers2', icon: 'color-palette' },
-    { title: 'salud', url: '/salud2',icon:'heart-half'},
-  ];
-  public appPages3 = [
-    { title: 'Ximopanolti', url: '/folder3/folder3.component', icon: 'home' },
-    { title: 'Kali', url: '/home3', icon: 'hand-left' },
-    { title: 'Tlapoua', url: '/numeros3', icon: 'calculator' },
-    { title: 'Tlapali', url: '/customers3', icon: 'color-palette' },
-    { title: 'salud', url: '/salud3',icon:'heart-half'},
-  ];
+public Kali =[
+  { title: 'Kali', url: '/home', icon: 'hand-left' },
+];
+public Tlapoua =[
+  { title: 'Tlapoua', url: '/numeros', icon: 'calculator' },
+];
+public Tlapali =[
+  { title: 'Tlapali', url: '/customers', icon: 'color-palette' },
+];
+public Pakayelistli =[
+  { title: 'Pakayelistli', url: '/salud',icon:'heart-half'},
+];
+public Kali2 =[
+  { title: 'Kali', url: '/home2', icon: 'hand-left' },
+];
+public Tlapoua2 =[
+  { title: 'Tlapoua', url: '/numeros2', icon: 'calculator' },
+];
+public Tlapali2 =[
+  { title: 'Tlapali', url: '/customers2', icon: 'color-palette' },
+];
+public salud2 =[
+  { title: 'salud', url: '/salud2',icon:'heart-half'},
+];
+public Kali3 =[
+  { title: 'Kali', url: '/home3', icon: 'hand-left' },
+];
+public Tlapoua3 =[
+  { title: 'Tlapoua', url: '/numeros3', icon: 'calculator' },
+];
+public Tlapali3 =[
+  { title: 'Tlapali', url: '/customers3', icon: 'color-palette' },
+];
+public salud3 =[
+  { title: 'salud', url: '/salud3',icon:'heart-half'},
+];
 }
