c225b7112ff7397ddf03aed59a8a6778acbca6f7
RibaldoD <ribaldodejesus@outlook.com>
2022-07-26 23:01:25 -0500

    Modificacion ts
    Funciones

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -8,8 +8,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  valor: string;
+  valor1: string;
+  valor2: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.valor = '0.50';
+    this.valor1 = '1';
+    this.valor2 = '0.75';
+   }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
